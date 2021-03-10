2fb46c27e6f6e88baf4fb425f841a804444d9522
per4month <per4month@gmail.com>
2021-03-10 11:37:11 +0700

    final class Rabbit

---
+++ src/Rabbit/IBehaviour.java
@@ -0,0 +1,9 @@
+package Rabbit;
+
+public interface IBehaviour {
+    void move(int x, int y);
+    int getX();
+    int getY();
+    void setX(int x);
+    void setY(int y);
+}
--- src/Rabbit/Rabbit.java
+++ src/Rabbit/Rabbit.java
@@ -1,6 +1,6 @@
 package Rabbit;
 
-public abstract class Rabbit {
+public abstract class Rabbit implements IBehaviour {
     public static int countOfRabbits = 0;
     private String pathToImg;
     private int x;
@@ -11,5 +11,34 @@ public abstract class Rabbit {
         this.y = y;
         this.pathToImg = pathToImg;
         //countOfRabbits++;
+    }
+
+    public String getPathToImg() {
+        return pathToImg;
+    }
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
     }
 }
