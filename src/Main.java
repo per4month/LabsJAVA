fc18672a3e0c077aeba033e9f99a29eb9e68d1b9
Roman Shevlyakov <romanshevly@gmail.com>
2021-03-10 11:34:35 +0700

    sad

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("Hallo");
         System.out.println("Hallo");
         System.out.println("Hallo");
+        System.out.println("Hallo");
+        System.out.println("Hallo");
 
     }
 }
