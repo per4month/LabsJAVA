a7e40e0c8f136b7ffb878816db809e9eb4eb41e8
per4month <per4month@gmail.com>
2021-03-10 11:43:45 +0700

    final class RabbitSimple

--- src/Rabbit/RabbitSimple.java
+++ src/Rabbit/RabbitSimple.java
@@ -1,5 +1,27 @@
 package Rabbit;
 
-public abstract class RabbitSimple {
-    public static int countOfSimple;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class RabbitSimple extends Rabbit {
+    public static int countOfSimple = 0;
+    static public Image image;
+
+    static {
+        try {
+            image = ImageIO.read(new File("src/resources/Ordinary.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    RabbitSimple(int x, int y, String pathToImg) {
+        super(x, y, pathToImg);
+        countOfSimple++;
+        countOfRabbits++;
+
+    }
 }
