17c3de96cae450afbc6890e0dfbd74e7e410e967
per4month <per4month@gmail.com>
2021-04-07 12:15:01 +0700

    Дополнил класс RabbitsStorage

--- src/Rabbit/RabbitStorage.java
+++ src/Rabbit/RabbitStorage.java
@@ -1,14 +1,25 @@
 package Rabbit;
 
+import Rabbit.RabbitAlbinos;
+import Rabbit.RabbitSimple;
+import Rabbit.Rabbit;
 
-import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.Vector;
 
 class RabbitsStorage {
-    private static RabbitsStorage instance;
-    private ArrayList<Rabbit> rabbitsList;
+    private static RabbitsStorage instance; //сущность - объект
+    //Коллекции
+    private Vector<Rabbit> rabbitsList; //для хранения кроликов
+    private TreeSet<String> aliveRabbits; //для хранения и поиска уникальных идентификаторов
+    private HashMap<String,Integer> rabbitsBornTime; //для хранения времени рония объектов
 
     private RabbitsStorage() {
-        this.rabbitsList = new ArrayList<>();
+        this.aliveRabbits = new TreeSet<>();
+        this.rabbitsBornTime = new HashMap<>();
+        this.rabbitsList = new Vector<>();
     }
 
     public static RabbitsStorage getInstance() {
@@ -18,7 +29,35 @@ class RabbitsStorage {
         return instance;
     }
 
-    public ArrayList<Rabbit> getRabbitsList() {
+    public Vector<Rabbit> getRabbitsList() {
         return rabbitsList;
+    }
+    public TreeSet<String> getAliveRabbits() {
+        return aliveRabbits;
+    }
+    public HashMap<String, Integer> getRabbitsBornTime() {
+        return rabbitsBornTime;
+    }
+    public void reset(){
+        instance.rabbitsBornTime = new HashMap<>();
+        instance.aliveRabbits = new TreeSet<>();
+        instance.rabbitsList = new Vector<>();
+    }
+    public void removeRabbits(int gameSec) {
+        for (int i = 0; i < rabbitsList.size(); i++) {
+            Rabbit rabbit = rabbitsList.get(i);
+            if (gameSec == rabbit.getDeathTime()) {
+                if (rabbit instanceof RabbitSimple) {
+                    RabbitSimple.countOfSimple--;
+                }
+                else {
+                    RabbitAlbinos.countOfAlbinos--;
+                }
+                Rabbit.countOfRabbits--;
+                rabbitsList.remove(rabbit);
+                aliveRabbits.remove(rabbit.getUUID());
+                rabbitsBornTime.remove(rabbit.getUUID());
+            }
+        }
     }
 }
